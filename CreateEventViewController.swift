72b90478decc33ab672ff2487a3440989ac07546
TonyAlb <albertya@mail.gvsu.edu>
2015-07-22 20:11:33 -0400

    Minor changes to storyboare

---
+++ CreateEventViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateEventViewController.swift
+//  whosin
+//
+//  Created by X Code User on 7/22/15.
+//  Copyright (c) 2015 Micha Schwendener. All rights reserved.
+//
+
+import UIKit
+
+class CreateEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
